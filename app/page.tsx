9196e7f85c6ff41e4bb9b9efc8def4b655125a8a
yavv17 <yaelangelvv@gmail.com>
2025-11-05 16:00:26 -0600

    Componente Watson

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import WatsonChat from "./src/components/WatsonChat";
+
 export default function Home() {
   return (
     <main style={{ textAlign: "center", padding: "2rem" }}>
       <h1>Bienvenido a Xeldrix Hackathon 🚀</h1>
       <p>Next.js ya está funcionando correctamente.</p>
+      <WatsonChat/>
     </main>
   );
 }
