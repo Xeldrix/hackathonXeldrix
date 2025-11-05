4cbf0f80f9dc0fcdd043f5ffab57f49bf8fc4b41
yavv17 <yaelangelvv@gmail.com>
2025-11-05 15:04:22 -0600

    Quitando la carpeta de next

--- .next/server/chunks/ssr/_de4e25a4._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[70864,a=>{a.n(a.i(33290))},43619,a=>{a.n(a.i(79962))},13718,a=>{a.n(a.i(85523))},18198,a=>{a.n(a.i(45518))},50708,a=>{"use strict";var b=a.i(7997);function c(){return(0,b.jsxs)("main",{style:{textAlign:"center",padding:"2rem"},children:[(0,b.jsx)("h1",{children:"Bienvenido a Xeldrix Hackathon 🚀"}),(0,b.jsx)("p",{children:"Next.js ya está funcionando correctamente."})]})}a.s(["default",()=>c])}];
-
-//# sourceMappingURL=_de4e25a4._.js.map
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XTm8Zx8JfqADVoKSugknmVQBFp4ME+ywxsBA2qdPu+U=\"\n}"
